0ce7b0098a300b72764371fcb00ebec081fd99d1
ritikgup-g <ritikgup1708@gmail.com>
2026-01-12 03:27:01 +0530

    Add About component for restaurant information

---
+++ About.jsx
@@ -0,0 +1,11 @@
+const About = () => (
+  <section id="about" className="section section-yellow about">
+    <div>
+      <h2 className="section-title">About Us</h2>
+      <p>YellowFork is a modern multi-cuisine restaurant.</p>
+      <p>Quality, hygiene & taste.</p>
+    </div>
+    <img src="https://images.unsplash.com/photo-1552566626-52f8b828add9" />
+  </section>
+);
+export default About;
